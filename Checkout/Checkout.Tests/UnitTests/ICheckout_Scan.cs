591d9a912e193394ec2759ac9ed0043729d440b2
pcmichaels <pcmichaels@live.com>
2017-03-18 10:19:45 +0000

    Add some initial unit tests

---
+++ Checkout/Checkout.Tests/UnitTests/ICheckout_Scan.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkout.Tests.UnitTests
+{
+    [TestFixture]
+    public class ICheckout_Scan
+    {
+        [Test]
+        public void SingleItemScanned()
+        {
+        }
+
+        [Test]
+        public void MultipleSingleItemsScanned()
+        {
+
+        }
+
+        [Test]
+        public void MultipleMixedItemsScanned()
+        {
+
+        }
+
+    }
+}
---
+++ Checkout/Checkout.Tests/UnitTests/ICheckout_TotalPrice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Checkout.Tests
+{
+    [TestFixture]
+    public class ICheckout_TotalPrice
+    {
+        [Test]
+        public void SingleItemScanned_TotalPrice()
+        {
+        }
+
+        [Test]
+        public void MultipleSingleItemsScanned_NoSpecialPrice()
+        {
+
+        }
+
+        [Test]
+        public void MultipleSingleItemsScanned_SpecialPrice()
+        {
+
+        }
+
+        [Test]
+        public void MultipleMixedItemsScanned_NoSpecialPrice()
+        {
+
+        }
+
+        [Test]
+        public void MultipleMixedItemsScanned_SpecialPrice()
+        {
+
+        }
+    }
+}
