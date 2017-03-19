89cb701805f4d83d7a0d4e0414abd1d51a7a9ca3
pcmichaels <pcmichaels@live.com>
2017-03-19 11:56:40 +0000

    Make the console app call the logic for a final test

--- Checkout/Checkout/Program.cs
+++ Checkout/Checkout/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Checkout.Core;
+using Checkout.Interface;
+using Checkout.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +13,23 @@ namespace Checkout
     {
         static void Main(string[] args)
         {
-            
+            PriceTable priceTable = PriceTableFactory.GetDefaultPriceTable();
+            PriceEngine priceEngine = new PriceEngine(priceTable);
+            ICheckout checkout = new Checkout.Core.Checkout(priceEngine);
+
+            while (true)
+            {
+                Console.Write("SKU: ");
+                string sku = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(sku))
+                {
+                    break;
+                }
+                checkout.Scan(sku);
+            }
+
+            Console.WriteLine($"Total price: {checkout.GetTotalPrice()}");
+            Console.ReadLine();
         }
     }
 }
